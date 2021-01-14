a79c59f0ca55b93ba6b0187c3164a67946b79a51
Joanna Stachowicz <joannastachowicz19@gmail.com>
2021-01-14 18:29:48 +0100

    Modify AppDescription component

--- app/script.js
+++ app/script.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
-// import AppDescription from './AppDescription.js';
 
+class AppDescription extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <p>According to optometrists in order to save your eyes, you should follow the 20/20/20.
+           It means you should to rest your eyes every 20 minutes for 20 seconds by looking more than 20 feet away.</p>
+        <p>This app will help you track your time and inform you when it's time to rest.</p>
+      </div>
+    );
+  }
+};
 class App extends React.Component {
   state = {
     status: 'off',
@@ -31,7 +42,7 @@ class App extends React.Component {
     return (
       <div>
         <h1>Protect your eyes</h1>
-        {(status === 'off') && <p>tekst</p>}
+        {(status === 'off') && <AppDescription />}
         {(status === 'work') && <img src="./images/work.png" />}
         {(status === 'rest') && <img src="./images/rest.png" />}
         {(status !== 'off') && <div className="timer">(time)</div>}
